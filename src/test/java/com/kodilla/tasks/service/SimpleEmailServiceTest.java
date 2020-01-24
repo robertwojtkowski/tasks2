51cb357cd6d2a0683b47bf73c8e70d7ea6a83905
robertwojtkowski <52374861+robertwojtkowski@users.noreply.github.com>
2020-01-24 11:47:34 +0100

    Add files via upload

---
+++ src/test/java/com/kodilla/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,39 @@
+package com.kodilla.tasks.service;
+
+import com.kodilla.tasks.domain.Mail;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+class SimpleEmailServiceTest {
+
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail(){
+        //given
+        Mail mail = new Mail("test@test.com", "Subject test", "testing message");
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        //when
+        simpleEmailService.send(mail);
+        //than
+        verify(javaMailSender, times(1)).send(mailMessage);
+    }
+
+}
