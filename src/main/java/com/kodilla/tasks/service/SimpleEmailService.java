9e1af466706c87979e832e2f4f9dd24273b03c14
robertwojtkowski <52374861+robertwojtkowski@users.noreply.github.com>
2020-01-28 06:27:59 +0100

    Update SimpleEmailService.java

--- src/main/java/com/kodilla/tasks/service/SimpleEmailService.java
+++ src/main/java/com/kodilla/tasks/service/SimpleEmailService.java
@@ -36,7 +36,7 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        if(!mail.getToCc().isEmpty())
+        if(mail.getToCc() != null)
             mailMessage.setCc(mail.getToCc());
         return mailMessage;
     }
